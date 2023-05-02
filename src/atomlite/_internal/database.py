3fb0feab36887230a8fdae84c9db11a3fdf86742
Lukas Turcani <lukasturcani93@gmail.com>
2023-05-02 13:16:06 +0100

    Lint

--- src/atomlite/_internal/database.py
+++ src/atomlite/_internal/database.py
@@ -1,19 +1,22 @@
-import typing
-import rdkit.Chem as rdkit
-import sqlite3
 import pathlib
+import sqlite3
+import typing
+
+import rdkit.Chem as rdkit
 
 
-class Entry(typing.TypedDict):
-    key: str  
-    molecule: rdkit.Mol
+class Entry(dict):
+    def __init__(self, key: str, molecule: rdkit.Mol) -> None:
+        super().__init__()
+        self["key"] = key
+        self["molecule"] = molecule
 
     @property
     def key(self) -> str:
         return self["key"]
 
     @property
-    def key(self) -> rdkit.Mol:
+    def molecule(self) -> rdkit.Mol:
         return self["molecule"]
 
 
@@ -21,11 +24,10 @@ class Database:
     connection: sqlite3.Connection
 
     def __init__(
-        self, 
+        self,
         database: pathlib.Path | str,
         molecule_table: typing.LiteralString = "molecules",
     ) -> None:
-
         self._molecule_table = molecule_table
         self.connection = sqlite3.connect(database)
         self.connection.enable_load_extension(True)
@@ -33,19 +35,21 @@ class Database:
         self.connection.enable_load_extension(False)
 
         self.connection.execute(
-            f"CREATE TABLE IF NOT EXISTS {molecule_table}(key TEXT PRIMARY KEY, molecule MOL)",
+            f"CREATE TABLE IF NOT EXISTS {molecule_table}("
+            "key TEXT PRIMARY KEY, molecule MOL)",
         )
 
     def add_molecules(self, molecules: Entry | typing.Iterable[Entry]) -> None:
         if isinstance(molecules, Entry):
-            molecules = (molecules, )
+            molecules = (molecules,)
 
         self.connection.executemany(
             f"INSERT INTO {self._molecule_table} VALUES (:key, :molecule)",
-            molecules
+            molecules,
         )
 
-    def get_molecules(self, keys: str | typing.Iterable[str]) -> typing.Iterator[Entry]:
+    def get_molecules(
+        self, keys: str | typing.Iterable[str]
+    ) -> typing.Iterator[Entry]:
         if isinstance(keys, str):
-            keys = (keys, )
-
+            keys = (keys,)
