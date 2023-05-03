90ed413090f246664e4f7626747e480759f7e4e8
Lukas Turcani <lukasturcani93@gmail.com>
2023-05-03 16:27:26 +0100

    Expose from_rdkit

--- src/atomlite/__init__.py
+++ src/atomlite/__init__.py
@@ -1,8 +1,9 @@
 from atomlite._internal.database import Database
-from atomlite._internal.json import Entry, to_rdkit
+from atomlite._internal.json import Entry, from_rdkit, to_rdkit
 
 __all__ = [
     "Database",
     "Entry",
     "to_rdkit",
+    "from_rdkit",
 ]
