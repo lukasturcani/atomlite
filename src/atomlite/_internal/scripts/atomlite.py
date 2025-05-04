0e63053600d7b36ca560f3b0c68de57d4bfb7f2f
Andrew Tarzia <andrew.tarzia@gmail.com>
2025-05-04 09:45:11 +0200

    Implement atomlite dump script for mol files. (#70)
    This commit implements a script into the atomlite module that
    allows the user to write a .mol file (using rdkit) from a database given
    an entry key.
    
    the command looks like:
    `atomlite dump molecules.db second second.mol`
    
    And this sets up the infrastructure for future atomlite scripts.

---
+++ src/atomlite/_internal/scripts/atomlite.py
@@ -0,0 +1,61 @@
+import argparse
+import logging
+import pathlib
+
+import rdkit.Chem as rdkit  # noqa: N813
+
+from atomlite._internal.database import Database
+from atomlite._internal.json import json_to_rdkit
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+)
+logger = logging.getLogger(__name__)
+
+
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="command", required=True)
+    dump = subparsers.add_parser(
+        "dump",
+        help="write a molecule from a database to a file (only .mol for now)",
+    )
+    dump.add_argument(
+        "database_path",
+        type=pathlib.Path,
+        help="path to atomlite database",
+    )
+    dump.add_argument("entry_key", help="entry key of molecule")
+    dump.add_argument(
+        "dump_path",
+        type=pathlib.Path,
+        help="path to file",
+    )
+
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = _parse_args()
+    if args.command == "dump":
+        if args.dump_path.suffix not in (".mol",):
+            msg = "only .mol files are supported"
+            raise RuntimeError(msg)
+
+        db = Database(args.database_path)
+        entry = db.get_entry(args.entry_key)
+        if entry is None:
+            msg = f"entry {args.entry_key} not found"
+            raise RuntimeError(msg)
+
+        rdkit.MolToMolFile(
+            mol=json_to_rdkit(entry.molecule),
+            filename=args.dump_path,
+            forceV3000=True,
+        )
+        logger.info("dumped %s to %s", args.entry_key, args.dump_path)
+
+
+if __name__ == "__main__":
+    main()
